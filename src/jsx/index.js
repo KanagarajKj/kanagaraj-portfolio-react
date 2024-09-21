b612bafb563870fa53f730aa354472a86f9a0da4
kanagaraj M <kanagarajkj98@gmail.com>
2024-09-21 13:32:15 +0530

    Code updated

--- src/jsx/index.js
+++ src/jsx/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import Navbar from './components/navbar/Navbar';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Skills from './components/skills/Skills';
-import Education from './components/education/Education';
+import Navbar from '../components/navbar/Navbar';
+import About from '../components/about/About';
+import Contact from '../components/contact/Contact';
+import Experience from '../components/experience/Experience';
+import Skills from '../components/skills/Skills';
+import Education from '../components/education/Education';
 
 const Index = () => {
   return (
